0e04d9d7f8941d0513985a5e50de49d49ec45807
Ksenyakin <30597080+Ksenyakin@users.noreply.github.com>
2023-01-24 18:32:16 +0300

    Update java8.js

--- java8.js
+++ java8.js
@@ -1,20 +1,94 @@
-var slapform = new Slapform();
-slapform.submit({
-    form: 'OtEoaDt65',
-    data: {
-        name: document.getElementById('name').innerHTML,
-        email: document.getElementById('email').innerHTML,
-        message: document.getElementById('message').innerHTML,
-    }
-})
-.then(function (response) {
-    console.log('Success', response);
-    alert('Success', response)
-})
-.catch(function (e) {
-    console.error('Fail', e);
-    alert('Fail', e)
-})
-.finally(function () { // This function runs regardless of success or error
-    console.log('This always runs!');
+function popup(par) {
+  let formDiv = document.getElementById("forma");
+  let bg = document.getElementById("popup_bg");
+  if (par === true) {
+    formDiv.style.display = ("block");
+    bg.style.opacity = ("1");
+    bg.style.pointerEvents = ("all");
+    history.pushState(true, null, "#form");
+  }
+
+  else {
+    formDiv.style.display = ("none");
+    bg.style.opacity = ("0");
+    bg.style.pointerEvents = ("none");
+    history.pushState(false, null, "#");
+  }
+}
+
+
+let names = document.getElementById("InputName");
+let email = document.getElementById("InputEmail");
+let msg = document.getElementById("Textarea");
+let cb = document.getElementById("Check");
+
+
+function save() {
+  localStorage.setItem("Имя", names.value);
+  localStorage.setItem("Почта", email.value);
+  localStorage.setItem("Сообщение", msg.value);
+  localStorage.setItem("Чекбокс", cb.checked);
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+  if (location.hash === "#form") {
+    popup(true);
+  }
+  window.addEventListener("popstate", (e) => {
+    popup(e);
   });
+  let b = document.getElementById("button1");
+  b.addEventListener("click", () => {
+    popup(true);
+  });
+  let c = document.getElementById("closebutton");
+  c.addEventListener("click", () => {
+    popup(false);
+  });
+  let popupBg = document.getElementById("popup_bg");
+  document.addEventListener("click", (e) => {
+    if (e.target === popupBg) {
+      popup(false);
+    }
+  });
+
+  names.value = localStorage.getItem("Имя");
+  email.value = localStorage.getItem("Почта");
+  msg.value = localStorage.getItem("Сообщение");
+  if (localStorage.getItem("Чекбокс") === "false"){
+    cb.checked = false;}
+  else{
+    cb.checked = true;}
+
+  names.oninput = save;
+  email.oninput = save;
+  msg.oninput = save;
+  cb.onclick = save;
+});
+
+$(function () {
+  $(".ajaxForm").submit(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: href,
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.status === "success") {
+          alert("Успешно!");
+          localStorage.removeItem("Имя");
+          localStorage.removeItem("Почта");
+          localStorage.removeItem("Сообщение");
+          localStorage.removeItem("Чекбокс");
+          names.value = localStorage.getItem("Имя");
+          email.value = localStorage.getItem("Почта");
+          msg.value = localStorage.getItem("Сообщение");
+          cb.checked = false;
+        } else {
+          alert("Ошибка! " + response.message);
+        }
+      }
+    });
+  });
+});
